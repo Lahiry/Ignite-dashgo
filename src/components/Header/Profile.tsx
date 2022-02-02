6f2e5fccb6c3ba3f6acb1893318addc2618b129b
Lahiry <raphaellcmb@al.insper.edu.br>
2022-02-02 20:01:56 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Raphael Lahiry</Text>
-        <Text color="gray.300" fontSize="small">
-          raphael.lahiry@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Raphael Lahiry</Text>
+          <Text color="gray.300" fontSize="small">
+            raphael.lahiry@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Raphael Lahiry" bg="pink.500" src="https://github.com/Lahiry.png" />
     </Flex>
